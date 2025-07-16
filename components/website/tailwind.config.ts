8cad8958043661191251232983f44f5323c76311
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2025-07-16 20:22:10 +0300

    chore: clean up style.css

--- components/website/tailwind.config.ts
+++ components/website/tailwind.config.ts
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          900: "#121212",
+          800: "#303030",
+          700: "#424242",
+          600: "#757575",
+          500: "#9e9e9e",
+          300: "#e0e0e0",
+          100: "#f5f5f5",
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
