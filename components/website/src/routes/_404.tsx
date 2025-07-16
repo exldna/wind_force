c62f95af111cf51aa6817fc890fa60a09e4f149b
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2025-07-16 23:55:53 +0300

    refactor: error pages

--- components/website/src/routes/_404.tsx
+++ components/website/src/routes/_404.tsx
@@ -7,30 +7,24 @@ export default function Error404Page() {
         <title>404 - Page not found</title>
       </Head>
 
-
-      <div className="min-h-screen bg-gradient-to-br from-blue-50 to-gray-100">
-        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-          <div className="text-center">
-            <p className="text-base font-semibold text-indigo-600">404</p>
-            <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+      <div class="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800">
+        <main class="grid min-h-full place-items-center px-6 py-24 sm:py-32">
+          <div class="text-center">
+            <p class="text-base font-semibold text-blue-400">404</p>
+            <h1 class="mt-4 text-5xl font-semibold tracking-tight text-gray-200 sm:text-7xl">
               Page not found
             </h1>
-            <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+            <p class="mt-6 text-lg font-medium text-gray-400 sm:text-xl">
               Sorry, we couldn’t find the page you’re looking for.
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="/"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
+            <div class="mt-10 flex justify-center">
+              <a href="/" class="btn-primary inline-flex items-center">
                 Go back home
               </a>
             </div>
           </div>
         </main>
       </div>
-
-
     </>
   );
 }
